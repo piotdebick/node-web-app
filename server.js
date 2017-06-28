7e2f8944f3041145694af1f016294e58537c167b
Piotr <piotrdebicki92@gmail.com>
2017-06-28 11:42:24 -0400

    Added projects page

--- server.js
+++ server.js
@@ -45,6 +45,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs',{
+    pageTitle: 'Portfolio Page',
+    welcomeMessage: 'Welcome to this here projects page'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Unable to handle request'
